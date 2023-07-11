522c7454722eddab3468a47ad072332d45e05561
Alexander Gängel <agaengel@users.noreply.github.com>
2023-07-11 15:55:22 +0200

    Loadtest 4. Run

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Loadtest 3. Run 
+                                    Loadtest 4. Run 
                                 </div>
                             </div>
                         </div>
