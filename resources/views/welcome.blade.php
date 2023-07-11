49b06674db8a0ef9665011697305505e6a5b7eec
Alexander Gängel <agaengel@users.noreply.github.com>
2023-07-11 14:58:35 +0200

    Update welcome.blade.php
    Loadtest 2. Run

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Loadtest 1. Run 
+                                    Loadtest 2. Run 
                                 </div>
                             </div>
                         </div>
